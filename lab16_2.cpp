c775b334795f10e42dea9fa699df9310e3212f1d
lab302-32 <lab302-32@labeng.local>
2020-03-03 17:36:10 +0700

    1

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 using namespace std;
 
-//Write the definition of myString() here
+void myString(char *&p,int N)
+{
+	p = new char [N];
+	for (int i=0;i<N;i++)
+	{
+		p[i] = 'A' + i;
+	}
 
+		p[N] = '\0';
+}
 int main(){
 	char *p;
 	int N;	
 	cout << "N = ";
 	cin >> N;		
 	myString(p,N);
-	cout << p;	
+	cout << p;
 	delete [] p;
 	return 0;
 }
